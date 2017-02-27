d08c4e018dbe2898693db9070715794afcd14a66
Abdulrahman Elsayed <abd.elsayed@hotmail.com>
2017-02-27 16:38:04 +0200

    Initial commit

---
+++ chapter-4/4_16c.c
@@ -0,0 +1,40 @@
+/*
+ ===============================================================================
+ Name        : 4_16a.c
+ Author      : Abdulrahman
+
+ Description : program that prints the following patterns separately,
+               one below the other. Use for loops to generate the patterns.
+               All asterisks (*) should be printed by a single printf statement
+               of the form printf( "%s", "*" ).
+Created on   : 27 Feb, 2017
+ ===============================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int row = 0;
+	int coloum = 0;
+	int height = 0 ;
+	printf("Enter the Height of the triangle:");
+	fflush(stdout);
+	scanf("%d",&height);
+	for (row = 0 ; row <height ;row++)
+		{
+			for (coloum = 0 ; coloum < height ; coloum++)
+			{
+				if (row <= coloum)
+				{
+			        printf("*");
+				}
+				else
+				{
+					printf(" ");
+				}
+			}
+			printf("\n");
+		}
+	return 0;
+}
---
+++ chapter-4/4_16d.c
@@ -0,0 +1,40 @@
+/*
+ ===============================================================================
+ Name        : 4_16d.c
+ Author      : Abdulrahman
+
+ Description : program that prints the following patterns separately,
+               one below the other. Use for loops to generate the patterns.
+               All asterisks (*) should be printed by a single printf statement
+               of the form printf( "%s", "*" ).
+ Created on  : 27 ,Feb 2017
+ ===============================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int row = 0;
+	int coloum = 0 ;
+	int height = 0 ;
+	printf("Enter the Height of the triangle:");
+	fflush(stdout);
+	scanf("%d",&height);
+	for (row = 1 ; row < height+1 ;row++)
+		{
+			for (coloum = height ; coloum > 0 ; coloum--)
+			{
+				if (row < coloum)
+				{
+			        printf(" ");
+				}
+				else
+				{
+					printf("*");
+				}
+			}
+			printf("\n");
+		}
+	return 0;
+}
