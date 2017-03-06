a02b0460a968f83656ad041f45fbe90bea7260e5
Abdulrahman Elsayed <abd.elsayed@hotmail.com>
2017-03-06 22:38:53 +0200

    Initil commit

---
+++ chapter-2/2_22.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : 2_22.c
+ Author      : Abdulrahman
+ Description : This code print pattern of stars.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+    printf( "*\n**\n***\n****\n*****\n" );
+    return 0;
+}
